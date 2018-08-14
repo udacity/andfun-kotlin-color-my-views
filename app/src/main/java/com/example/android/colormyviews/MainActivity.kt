da1bb9b94d3683265f27195199876f45400d3893
assersamak <mr.fuzzz@gmail.com>
2018-08-14 19:32:32 +1000

    Step.03-Solution-Basline-constraint-Button-chain

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -37,7 +37,8 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val clickableViews: List<View> =
                 listOf(box_one_text, box_two_text, box_three_text,
-                        box_four_text, box_five_text)
+                        box_four_text, box_five_text, constraint_layout,
+                        red_button, green_button, yellow_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -65,7 +66,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
 
-            // TODO (03) Update the click handler to handle the button clicks for color changes.
+        // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
