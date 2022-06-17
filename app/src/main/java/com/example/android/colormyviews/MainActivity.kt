2dfff9cf09eac5f3e2b2f0d9f7013251fc96e902
SudKul <sudhanshu.kulshrestha@gmail.com>
2022-06-17 12:48:10 +0530

    Upgrade to AndroidX.

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -18,16 +18,17 @@ package com.example.android.colormyviews
 
 import android.graphics.Color
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setListeners()
     }
 
